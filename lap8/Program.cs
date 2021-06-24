738585d60462a02d675c03fee05c8ffa64ee57e2
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-06-24 16:56:05 +0700

    added lap 8

---
+++ lap8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lap8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var animalGeneric = new AnimalGeneric<Animal>();
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            animalGeneric.Add(new Cat
+            {
+                Id = 12,
+                Name = "niu",
+                Dob = "hello"
+            });
+            animalGeneric.Add(new Dog()
+            {
+                Id = 12,
+                Name = "lag",
+                Dob = "ga"
+            });
+            
+            animalGeneric.ShowInformation();
+
+
+
+
+        }
+    }
+}
